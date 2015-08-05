2a27c752ad1fcd7c13b255a94edcd8c133a02e2d
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-08-04 21:21:46 -0500

    Updated to support cheerio for HTML parse and got the final request to automatically run
    The code is still a mess, need to add a schedule and make it promise based and configurable but the core idea is there
    
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var https = require('https');
 var cookie = require('cookie');
 var querystring = require('querystring');
+var cheerio = require('cheerio');
 var server = http.createServer(function(req, res) {
     if (req.url === '/favicon.ico') {
         res.writeHead(200, {
@@ -13,59 +14,68 @@ var server = http.createServer(function(req, res) {
     }
     var initReq = https.get("https://disneyworld.disney.go.com/dining/magic-kingdom/be-our-guest-restaurant/", function(res) {
         var sessionCookie = cookie.parse(res.headers['set-cookie'].join(';'))['PHPSESSID'];
-        sessionCookie = 'at5ki0qibtkc19htuobj9sepb2';
+        // sessionCookie = '19bgblu2834sda6m7vlf6vt0b2';
         console.log(sessionCookie);
-        var postData = querystring.stringify({
-        	// TODO: Need to pull this from the above request HTML response
-            pep_csrf: '0c5e143929d14eb707be68af219598ca286eac692114d10f36a1fea4344d0d35502347be602cc8e4d5dba30b4e0eeeb6c3beb3deef17395530a43a4d4b6f76a4',
-            searchDate: '2015-08-04',
-            skipPricing: true,
-            searchTime: '08:00',
-            partySize: 2,
-            id: '16660079;entityType=restaurant',
-            type: 'dining'
+        var data = '';
+        res.on("data", function(chunk) {
+            data += chunk;
         });
-        var options = {
-            host: 'disneyworld.disney.go.com',
-            path: '/finder/dining-availability/',
-            method: 'POST',
-            headers: {
-            	// these are the two things you definitely need
-            	// the s_vi one looks like it just needs to have been created at some point, keep alive is a couple years so don't need to try and get it each time
-                'Cookie': 'PHPSESSID=at5ki0qibtkc19htuobj9sepb2; s_vi=[CS]v1|2AE01B6C05012E2B-4000013760054F62[CE]',
-                // need these otherwise it 302
-                Host: 'disneyworld.disney.go.com',
-                Origin: 'https://disneyworld.disney.go.com',
-                Referer: 'https://disneyworld.disney.go.com/dining/magic-kingdom/be-our-guest-restaurant/',
-                // end need
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': postData.length,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
-                Accept: '*/*',
-                // TODO: figure out how we can accept gzipping to be nice
-                // 'Accept-Encoding':'gzip, deflate',
-                'Accept-Language':'en-US,en;q=0.8',
-                'X-Requested-With': 'XMLHttpRequest',
-                'X-NewRelic-ID':'Uw4BWVZSGwUCXFVVBwI='
+        res.on('end', function() {
+            $ = cheerio.load(data);
+
+            var csrfToken = $('#pep_csrf').val();
+            console.log(csrfToken);
+            var postData = querystring.stringify({
+                pep_csrf: csrfToken,
+                searchDate: '2015-08-05',
+                skipPricing: true,
+                searchTime: '22:00',
+                partySize: 2,
+                id: '16660079;entityType=restaurant',
+                type: 'dining'
+            });
+            var options = {
+                host: 'disneyworld.disney.go.com',
+                path: '/finder/dining-availability/',
+                method: 'POST',
+                headers: {
+                    // these are the two things you definitely need
+                    // the s_vi one looks like it just needs to have been created at some point, keep alive is a couple years so don't need to try and get it each time
+                    'Cookie': 'PHPSESSID=' + sessionCookie + '; s_vi=[CS]v1|2AE01B6C05012E2B-4000013760054F62[CE]',
+                    // need these otherwise it 302
+                    Host: 'disneyworld.disney.go.com',
+                    Origin: 'https://disneyworld.disney.go.com',
+                    Referer: 'https://disneyworld.disney.go.com/dining/magic-kingdom/be-our-guest-restaurant/',
+                    // end need
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': postData.length,
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+                    Accept: '*/*',
+                    // TODO: figure out how we can accept gzipping to be nice
+                    // 'Accept-Encoding':'gzip, deflate',
+                    'Accept-Language': 'en-US,en;q=0.8',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-NewRelic-ID': 'Uw4BWVZSGwUCXFVVBwI='
+                }
+            };
+            callback = function(response) {
+                var str = ''
+                response.on('data', function(chunk) {
+                    str += chunk;
+                });
+                response.on('end', function() {
+                    console.log(response.statusCode);
+                    console.log(response.statusMessage);
+                    console.log(str);
+                });
             }
-        };
-        callback = function(response) {
-            var str = ''
-            response.on('data', function(chunk) {
-                str += chunk;
+            var wdwReq = https.request(options, callback).on('error', function(err) {
+                console.log(err);
             });
-            response.on('end', function() {
-                console.log(response.statusCode);
-                console.log(response.statusMessage);
-                console.log(str);
-            });
-        }
-        var wdwReq = https.request(options, callback).on('error', function(err) {
-            console.log(err);
+            //This is the data we are posting, it needs to be a string or a buffer
+            wdwReq.write(postData);
+            wdwReq.end();
         });
-        //This is the data we are posting, it needs to be a string or a buffer
-        wdwReq.write(postData);
-        wdwReq.end();
     }).on('error', function(e) {
         console.log("Got error: " + e.message);
     });
