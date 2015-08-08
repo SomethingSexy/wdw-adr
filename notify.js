3a29f291af9f0dcbd9f3aafcdb4119e07e8a43d6
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-08-07 21:08:46 -0500

    Updated to properly process from notification.js
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- notify.js
+++ notify.js
@@ -1,6 +1,7 @@
 'use strict';
 import nodemailer from 'nodemailer';
 import emailConfig from './config/email';
+import notificationConfig from './config/notification';
 
 // setup email transporter
 const transporter = nodemailer.createTransport({
@@ -9,18 +10,28 @@ const transporter = nodemailer.createTransport({
     auth: emailConfig.auth
 });
 // given a response, used to notify
-var notify = function(response) {
-    console.log('notifying');
+let notify = function(response, person) {
+    transporter.sendMail({
+        from: emailConfig.from,
+        to: person.email,
+        subject: 'Reservation - ' + response.title,
+        text: response.notification.bodyText,
+        html: response.notification.bodyHtml
+    });
+};
+
+let processResponse = function(response){
     if (response) {
-        transporter.sendMail({
-            from: 'admin@collectionstash.com',
-            to: 'tyler.cvetan@gmail.com',
-            subject: 'Reservation - ' + response.title,
-            text: response.notification.bodyText,
-            html: response.notification.bodyHtml
-        });
+        console.log('notifying');
+        // see who is configured for this reservation
+        if(Array.isArray(response.notify)){
+            response.notify.forEach((element, index, array) => {
+                notify(response, notificationConfig[element]);
+            });
+        }
     }
+
     return response;
 };
 
-export default notify;
+export default processResponse;
