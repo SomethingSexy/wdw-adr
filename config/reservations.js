cab04aac2b4324d403606f77da8990d2db4453be
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-08-07 21:12:58 -0500

    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

---
+++ config/reservations.js
@@ -0,0 +1,18 @@
+'use strict';
+export default [{
+	id : '16660079;entityType=restaurant',
+	title: 'Be Our Guest Restaurant',
+	url: 'https://disneyworld.disney.go.com/dining/magic-kingdom/be-our-guest-restaurant/',
+	time: 'dinner',
+	date: '2015-11-06',
+	partySize: 2,
+	notify : ['kelly', 'tyler']
+},{
+	id : '90001248;entityType=restaurant',
+	title: 'Artist Point',
+	url: 'https://disneyworld.disney.go.com/dining/wilderness-lodge-resort/artist-point/',
+	time: '21:00',
+	date: '2015-08-08',
+	partySize: 2,
+	notify : ['tyler']
+}];
