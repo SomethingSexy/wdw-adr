36ca19d531691aa3d45eb37f939921e99548264e
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-08-15 12:35:31 -0500

    Updated eslint errors
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- dining/dining.js
+++ dining/dining.js
@@ -12,24 +12,26 @@ import merge from 'merge';
 const getSessionData = function(reservation) {
   return Q.Promise(function(resolve, reject) {
     console.log('running session call');
+
     let initReq = https.get(reservation.url, function(res) {
       const sessionCookie = cookie.parse(res.headers['set-cookie'].join(';'))['PHPSESSID'];
-      // console.log(sessionCookie);
       let data = '';
+
       res.on('data', function(chunk) {
-          data += chunk;
+        data += chunk;
       });
+      
       res.on('end', function() {
-          let $ = cheerio.load(data);
-          let csrfToken = $('#pep_csrf').val();
-          // add the necessary data to the reservation
-          resolve(merge(true, {
-              sessionCookie: sessionCookie,
-              csrfToken: csrfToken
-          }, reservation));
+        let $ = cheerio.load(data);
+        let csrfToken = $('#pep_csrf').val();
+        // add the necessary data to the reservation
+        resolve(merge(true, {
+          sessionCookie: sessionCookie,
+          csrfToken: csrfToken
+        }, reservation));
       });
     }).on('error', function(e) {
-        reject(new Error('cannot retrieve session data ' + e.message));
+      reject(new Error('cannot retrieve session data ' + e.message));
     });
   });
 };
@@ -39,16 +41,16 @@ const getSessionData = function(reservation) {
 const getReservationData = function(reservation) {
   return Q.Promise(function(resolve, reject) {
     console.log('running reservation call');
-    var time;
+    let time;
     if (reservation.time === 'dinner') {
-        time = '80000714';
+      time = '80000714';
     } if(reservation.time === 'breakfast'){
-        time = '80000712';
+      time = '80000712';
     } else {
-        time = reservation.time;
+      time = reservation.time;
     }
-    
-    var postData = querystring.stringify({
+
+    let postData = querystring.stringify({
       pep_csrf: reservation.csrfToken,
       searchDate: reservation.date,
       skipPricing: true,
@@ -57,44 +59,46 @@ const getReservationData = function(reservation) {
       id: reservation.id,
       type: 'dining'
     });
-    var options = {
+
+    const options = {
       host: 'disneyworld.disney.go.com',
       path: '/finder/dining-availability/',
       method: 'POST',
       headers: {
-          // these are the two things you definitely need
-          // the s_vi one looks like it just needs to have been created at some point, keep alive is a couple years so don't need to try and get it each time
-          'Cookie': 'PHPSESSID=' + reservation.sessionCookie + '; s_vi=[CS]v1|2AE01B6C05012E2B-4000013760054F62[CE];',
-          // need these otherwise it 302
-          Host: 'disneyworld.disney.go.com',
-          Origin: 'https://disneyworld.disney.go.com',
-          Referer: reservation.url,
-          // end need
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': postData.length,
-          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
-          Accept: '*/*',
-          // TODO: figure out how we can accept gzipping to be nice
-          // 'Accept-Encoding':'gzip, deflate',
-          'Accept-Language': 'en-US,en;q=0.8',
-          'X-Requested-With': 'XMLHttpRequest'
+        // these are the two things you definitely need
+        // the s_vi one looks like it just needs to have been created at some point, keep alive is a couple years so don't need to try and get it each time
+        'Cookie': 'PHPSESSID=' + reservation.sessionCookie + '; s_vi=[CS]v1|2AE01B6C05012E2B-4000013760054F62[CE];',
+        // need these otherwise it 302
+        Host: 'disneyworld.disney.go.com',
+        Origin: 'https://disneyworld.disney.go.com',
+        Referer: reservation.url,
+        // end need
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': postData.length,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+        Accept: '*/*',
+        // TODO: figure out how we can accept gzipping to be nice
+        // 'Accept-Encoding':'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'X-Requested-With': 'XMLHttpRequest'
       }
     };
-    var callback = function(response) {
-      var str = ''
+
+    const callback = function(response) {
+      let str = '';
       response.on('data', function(chunk) {
           str += chunk;
       });
       response.on('end', function() {
-          console.log(response.statusCode);
-          // add the raw response to the reservation data
-          resolve(merge(true, {
-              rawData: str,
-          }, reservation));
+        console.log(response.statusCode);
+        // add the raw response to the reservation data
+        resolve(merge(true, {
+          rawData: str,
+        }, reservation));
       });
     };
 
-    var wdwReq = https.request(options, callback).on('error', function(err) {
+    const wdwReq = https.request(options, callback).on('error', function(err) {
       console.log(err);
     });
 
@@ -108,39 +112,39 @@ const getReservationData = function(reservation) {
  */
 const isReservationAvailable = function(reservation) {
   return Q.Promise(function(resolve, reject) {
-      console.log('processing response');
-      console.log(reservation.rawData);
-      let $ = cheerio.load(reservation.rawData);
-      if (!$('#diningAvailabilityFlag').data('hasavailability')) {
-          console.log('no availablity');
-          resolve(false);
-      } else {
-          // assuming there are some times available
-          // now grab the actual available times
-          var times = $('.pillLink', '.ctaAvailableTimesContainer').get().map((el) => {
-              return $('.buttonText', el).text();
-          });
-          // add the results to the reservation data
-          const searchText = $('.diningReservationInfoText.available').text().trim();
-          // use the results for just the raw data
-          // notification will be the data used if a notification is being sent
-          console.log('has availablity');
-          resolve(merge(true, {
-              results: {
-                  times: times,
-                  searchText: searchText
-              },
-              notification: {
-                  subject: reservation.title,
-                  bodyText: searchText + '\r\n' + times.join(' '),
-                  bodyHtml: searchText + '<br\><br\>' + times.join(' ')
-              }
-          }, reservation));
-      }
+    console.log('processing response');
+    console.log(reservation.rawData);
+    let $ = cheerio.load(reservation.rawData);
+    if (!$('#diningAvailabilityFlag').data('hasavailability')) {
+      console.log('no availablity');
+      resolve(false);
+    } else {
+      // assuming there are some times available
+      // now grab the actual available times
+      var times = $('.pillLink', '.ctaAvailableTimesContainer').get().map((el) => {
+          return $('.buttonText', el).text();
+      });
+      // add the results to the reservation data
+      const searchText = $('.diningReservationInfoText.available').text().trim();
+      // use the results for just the raw data
+      // notification will be the data used if a notification is being sent
+      console.log('has availablity');
+      resolve(merge(true, {
+        results: {
+          times: times,
+          searchText: searchText
+        },
+        notification: {
+          subject: reservation.title,
+          bodyText: searchText + '\r\n' + times.join(' '),
+          bodyHtml: searchText + '<br\><br\>' + times.join(' ')
+        }
+      }, reservation));
+    }
   });
 };
 
 export default function(reservation) {
     // run
   return getSessionData(reservation).then(getReservationData).then(isReservationAvailable);
-};
+}
